27d35c266cf194abf0b6b8fc3e1507216e7bc04f
michelrubens <michelrube@gmail.com>
2023-04-05 22:19:47 -0300

    Update Repository

--- observer/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args){
-        Editora e = new Editora();
-
-        e.addObserver(true);
-    }
-}
--- observer/src/Publicacao.java
+++
@@ -1,5 +0,0 @@
-public interface Publicacao {
-    public void addObserver(Observer o);
-    public void removeObserver(Observer o);
-    public void notifyObservers(boolean novaPublicacao);
-}
