28071ce27762d9b916e9ae20ae31360e9f6f4445
Michel Rubens <michelrube@gmail.com>
2023-06-12 21:35:19 -0300

    Atualização 21h35

--- padroes/mvc-facade-singleton/src/facade/EcommerceFacade.java
+++ padroes/mvc-facade-singleton/src/facade/EcommerceFacade.java
@@ -1,27 +1,26 @@
 package facade;
 
-import mvc.Controller;
 import mvc.Model;
 import mvc.View;
 
 public class EcommerceFacade {
-    private Controller controller;
+    private Model model;
+    private View view;
 
     public EcommerceFacade() {
-        Model model = Model.getInstance();
-        View view = new View();
-        controller = new Controller(model, view);
+        model = Model.getInstance();
+        view = new View();
     }
 
     public void addItemToCart(String item) {
-        controller.addItemToCart(item);
+        model.addItemToCart(item);
     }
 
     public void removeItemFromCart(String item) {
-        controller.removeItemFromCart(item);
+        model.removeItemFromCart(item);
     }
 
     public void displayCartItems() {
-        controller.displayCartItems();
+        view.displayCartItems(model.getCartItems());
     }
 }
